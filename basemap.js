c349db27384007e2f1f7c7d49655660cb6a3193a
Segat <david.segat@student.uibk.ac.at>
2018-04-18 16:50:15 +0200

    Basemap hinzugefügt

--- basemap.js
+++ basemap.js
@@ -1,1 +1,6 @@
-alert("Hallo Welt");
+
+let myMap = L.map("mapdiv");
+ let myLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+
+myMap.addLayer(myLayer);
+myMap.setView([47.267,11.383], 11);
