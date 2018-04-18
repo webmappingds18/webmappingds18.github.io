bdd333a5b4876c3f8c6983db9ba20ce60054843f
Segat <david.segat@student.uibk.ac.at>
2018-04-18 18:19:04 +0200

    quelle als link hinzufügen

--- basemap.js
+++ basemap.js
@@ -4,21 +4,27 @@ let myMap = L.map("mapdiv");
 
 let myLayers = {
 
+
 osm : L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
 
-geolandbasemap : L.tileLayer("https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"),
+geolandbasemap : L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+subdomains : ["maps","maps1","maps2","maps3","maps4"],
+attribution : "Datenquelle: <a href=' https://www.basemap.at'>basemap.at</a>"
+     }
+),
 
-bmapoverlay : L.tileLayer("https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"),
 
-bmaporthofoto30cm : L.tileLayer("https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"),
+bmapoverlay : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"),
 
-bmaphidpi : L.tileLayer("https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"),
+bmaporthofoto30cm : L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"),
 
-bmapgrau : L.tileLayer("https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"),
+bmaphidpi : L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"),
+
+bmapgrau : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"),
 
 };
 
-myMap.addLayer(myLayers.bmaporthofoto30cm);
+myMap.addLayer(myLayers.geolandbasemap);
 myMap.setView([47.267,11,383], 11);
 
 
